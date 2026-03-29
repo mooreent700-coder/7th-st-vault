b6eb11701c15d1237afaefde68debba7c7f2046e
Vena Myles <venamyles@Mac.lan>
2026-03-29 00:20:59 -0700

    fix stripe api version

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,9 +1,8 @@
-
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2026-03-25.dahlia",
 });
 
 export async function POST(req: Request) {
