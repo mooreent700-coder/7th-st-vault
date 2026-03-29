e7c5c6bd11da504110156883197d1b8a7050f992
Vena Myles <venamyles@Mac.lan>
2026-03-29 00:25:27 -0700

    fix webhook stripe api version

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { headers } from "next/headers";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
+  apiVersion: "2026-03-25.dahlia",
 });
 
 export async function POST(req: Request) {
